9bf35998390a52df8e616cd3bc611d08752f15d1
Rukshi Prissilla <289871842+RukshiPrissilla@users.noreply.github.com>
2026-06-18 06:52:09 +0530

    change in list

--- list.py
+++ list.py
@@ -20,3 +20,33 @@ print (numbers)  #[5,6,7,8,4]
 del numbers[1]
 print(numbers)  #[5,7,8,4]
 print(numbers[2:])  #[8,4]
+
+#multidimensional list
+data=[[1,1,1],[2,2,2],[3,3,3]]
+print(data[1][1])  #2
+data[1][1]=25
+print(data)  #[[1,1,1],[2,25,2],[3,3,3]]
+data[1].append(3)
+print(data)  #[[1,1,1],[2,25,2,3],[3,3,3]]
+
+#length
+print(len([1,2,3]))  #3
+
+#concatenation
+a=[1,2,3]
+b=[4,5,6]
+print(a+b) #[1,2,3,4,5,6]
+
+#repetition
+print(["hi"]*2)  #['hi','hi']
+
+#membership&iteration
+print(3in[1,2,3]) #True
+for x in [1,2,3]:
+    print(x,end=" ") #1 2 3
+
+#indexing&slicing
+K=['s','d','g']
+#K[2]=g
+#K[-2]=d
+#K[1:]=d,g
